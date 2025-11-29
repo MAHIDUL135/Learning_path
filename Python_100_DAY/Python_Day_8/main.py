a2f4e9baea3d763b178444c981ca1675885b09f5
MAHIDUL135 <mahidulahasan135@gmail.com>
2025-11-29 21:33:40 +0530

    ok

---
+++ Python_100_DAY/Python_Day_8/main.py
@@ -0,0 +1,12 @@
+a = 5
+b = 3
+
+print("The sum of", a, "and", b, "is", a + b)
+print("The difference when", b, "is subtracted from", a, "is", a - b)
+print("The product of", a, "and", b, "is", a * b)
+print("The quotient when", a, "is divided by", b, "is", a / b)
+# print("The remainder when", a, "is divided by", b, "is", a % b)
+# print("The", a, "raised to the power of", b, "is", a ** b)
+# print("The floor division of", a, "by", b, "is", a // b)
+
+# This is a simple calculator that performs basic arithmetic operations
