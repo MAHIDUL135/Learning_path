1c76c26eedae1aba77043d8084004faff8de93eb
MAHIDUL135 <mahidulahasan135@gmail.com>
2025-11-29 21:31:12 +0530

    update

---
+++ Python_100_DAY/Python_Day_7/main.py
@@ -0,0 +1,18 @@
+print(5+6)
+print(10-2)
+print(4*2)
+print(16/2)
+print(17%3)
+print(2**3)
+print(15//2)
+
+a = 10
+b = 5
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)
+print(a//b)
